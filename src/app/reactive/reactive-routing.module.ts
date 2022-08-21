cdc627c6ef35b737da81dbe44cf813f74e82c40e
lucio-crDEV <lucio.m.cardenas@gmail.com>
2022-08-21 03:08:55 -0400

    reactive routing structure

--- src/app/reactive/reactive-routing.module.ts
+++ src/app/reactive/reactive-routing.module.ts
@@ -2,14 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
 
 const routes: Routes = [
 {
   path: '',
   children: [
     { path: 'basicos', component: BasicosComponent },
-    { path: 'dinamicos', component: BasicosComponent },
-    { path: 'switches', component: BasicosComponent },
+    { path: 'dinamicos', component: DinamicosComponent },
+    { path: 'switches', component: SwitchesComponent },
     { path: '**', redirectTo: 'basicos' }
   ]
 }
